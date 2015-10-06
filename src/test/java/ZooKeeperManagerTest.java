540f04c16dc70c80b39072a25f931d1c72ce226c
Juan Jesús Chorro <jjchorro.ext@redborder.net>
2015-10-06 12:13:19 +0200

    Added malware testing. Fix some errors with MalwareRevisionList.java

--- src/test/java/ZooKeeperManagerTest.java
+++ src/test/java/ZooKeeperManagerTest.java
@@ -4,63 +4,70 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.RetryNTimes;
 import org.apache.curator.test.TestingServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
 @RunWith(MockitoJUnitRunner.class)
 public class ZooKeeperManagerTest extends TestCase {
 
+    TestingServer zooKeeperServer;
 
-    @Test
-    public void connectCorrectly(){
-
-        TestingServer zooKeeperServer;
-
+    @Before
+    public void init() {
         try {
-
             zooKeeperServer = new TestingServer();
             zooKeeperServer.start();
-
-            CuratorFramework curatorClient = CuratorFrameworkFactory.newClient("localhost:"+zooKeeperServer.getPort(),new RetryNTimes(Integer.MAX_VALUE, 30000));
-
-            assertNotNull(curatorClient);
-
-            zooKeeperServer.close();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
-    @Test
-    public void createPathCorrectly(){
-        TestingServer zooKeeperServer;
-
+    @After
+    public void shutdown() {
         try {
-
-            zooKeeperServer = new TestingServer();
-            zooKeeperServer.start();
-
-            CuratorFramework curatorClient = CuratorFrameworkFactory.newClient("localhost:"+zooKeeperServer.getPort(),new RetryNTimes(Integer.MAX_VALUE, 30000));
-            curatorClient.start();
-
-            ZkUtils zkUtils = new ZkUtils(curatorClient, "/Proof");
-            zkUtils.registerNode();
-
-            assertNotNull(zkUtils);
-
-            zkUtils.createNodeIfNotExist("node1");
-
-            assertTrue(zkUtils.checkIfExist("/Proof/node1"));
-
             zooKeeperServer.close();
-
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    @Test
+    public void connectCorrectly() {
+
+        CuratorFramework curatorClient = CuratorFrameworkFactory.newClient("localhost:" + zooKeeperServer.getPort(), new RetryNTimes(Integer.MAX_VALUE, 30000));
+        assertNotNull(curatorClient);
+
+
+    }
+
+    @Test
+    public void createPathCorrectly() {
+
+        CuratorFramework curatorClient = CuratorFrameworkFactory.newClient("localhost:" + zooKeeperServer.getPort(), new RetryNTimes(Integer.MAX_VALUE, 30000));
+        curatorClient.start();
+
+        ZkUtils zkUtils = null;
+
+        try {
+            zkUtils = new ZkUtils(curatorClient, "/Proof");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        zkUtils.registerNode();
+
+        assertNotNull(zkUtils);
+
+        zkUtils.createNodeIfNotExist("node1");
+
+        assertTrue(zkUtils.checkIfExist("/Proof/node1"));
+    }
 
 }
