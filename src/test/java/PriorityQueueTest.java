b78e4ca01766bc31c5a94f3783859b62f2a05b92
Juan Jesús Chorro <jjchorro.ext@redborder.net>
2015-10-22 11:22:50 +0200

    Added PriorityQueueTest. Modified DataModule interface in getIncremental method, now return Object type class. Added write method in KafkaOutputModule

---
+++ src/test/java/PriorityQueueTest.java
@@ -0,0 +1,83 @@
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Comparator;
+import java.util.concurrent.PriorityBlockingQueue;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class PriorityQueueTest extends TestCase {
+
+    PriorityBlockingQueue<Service> queue;
+
+    @Before
+    public void init(){
+        queue = new PriorityBlockingQueue<>(1000, new ServiceComparator());
+
+        queue.offer(new Service(1,true));
+        queue.offer(new Service(2,true));
+        queue.offer(new Service(3,true));
+
+    }
+
+    @Test
+    public void sizeTest(){
+        Service refService = queue.peek();
+        assertEquals(1, refService.getPriority());
+        assertTrue(refService.isReady());
+        assertEquals(3, queue.size());
+
+        System.out.println(queue.toString());
+    }
+
+    @Test
+    public void valueTest(){
+        Service refService = queue.peek();
+        assertEquals(1, refService.getPriority());
+        assertTrue(refService.isReady());
+    }
+
+    private class Service {
+        private int priority;
+        private boolean ready;
+
+        public Service(int priority, boolean ready){
+            this.priority = priority;
+            this.ready = ready;
+        }
+
+        public int getPriority(){
+            return priority;
+        }
+
+        public boolean isReady(){
+            return ready;
+        }
+
+        public String toString(){
+            return "Priority : " + priority + " & Ready : " + (ready ? 1 : 0);
+        }
+    }
+
+    private class ServiceComparator implements Comparator<Service> {
+
+        @Override
+        public int compare(Service o1, Service o2) {
+
+            System.out.println(o1.toString());
+            System.out.println(o2.toString());
+            int priority = o1.getPriority() - o2.getPriority();
+            int ready = (o1.isReady() ? 1 : 0) - (o2.isReady() ? 1 : 0);
+            System.out.println("Priority : " + priority);
+            System.out.println("Ready : " + ready);
+
+            return o1.getPriority() - o2.getPriority();
+        }
+
+    }
+}
